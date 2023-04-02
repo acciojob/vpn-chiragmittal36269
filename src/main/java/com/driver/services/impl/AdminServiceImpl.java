c97bcb8453af9f6f9a927e9ae121c60994325d2a
Chirag Mittal <chiragmittal36269@gmail.com>
2023-04-02 23:52:57 +0530

    A

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -2,7 +2,6 @@ package com.driver.services.impl;
 
 import com.driver.model.Admin;
 import com.driver.model.Country;
-import com.driver.model.CountryName;
 import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
@@ -57,47 +56,48 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception {
         String s = countryName.toUpperCase();
-        boolean marker;
-        marker = s.equals("IND") || s.equals("USA") || s.equals("AUS") || s.equals("CHI") || s.equals("JPN");
+//        boolean marker;
+//        marker = s.equals("IND") || s.equals("USA") || s.equals("AUS") || s.equals("CHI") || s.equals("JPN");
+//
+//        if (!marker) {
+//            throw new Exception("Country not found");
+//        }
 
-        if (!marker) {
+        if (!s.equals("IND") && !s.equals("USA") && !s.equals("AUS") && !s.equals("CHI") && !s.equals("JPN")) {
             throw new Exception("Country not found");
         }
-
-//        if (!countryNameUpperCase.equals("IND") && !countryNameUpperCase.equals("USA") && !countryNameUpperCase.equals("AUS") && !countryNameUpperCase.equals("CHI") && !countryNameUpperCase.equals("JPN")) throw new Exception("Country not found");
-
 
         ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
 
         Country country = new Country();
 
-        switch (s) {
-            case "IND":
-                country.setCountryName(CountryName.IND);
-                country.setCode(CountryName.IND.toCode());
-                break;
-            case "USA":
-                country.setCountryName(CountryName.USA);
-                country.setCode(CountryName.USA.toCode());
-                break;
-            case "AUS":
-                country.setCountryName(CountryName.AUS);
-                country.setCode(CountryName.AUS.toCode());
-                break;
-            case "CHI":
-                country.setCountryName(CountryName.CHI);
-                country.setCode(CountryName.CHI.toCode());
-                break;
-            case "JPN":
-                country.setCountryName(CountryName.JPN);
-                country.setCode(CountryName.JPN.toCode());
-                break;
-            default :
-                throw new Exception("Country not found");
-        }
+//        switch (s) {
+//            case "IND":
+//                country.setCountryName(CountryName.IND);
+//                country.setCode(CountryName.IND.toCode());
+//                break;
+//            case "USA":
+//                country.setCountryName(CountryName.USA);
+//                country.setCode(CountryName.USA.toCode());
+//                break;
+//            case "AUS":
+//                country.setCountryName(CountryName.AUS);
+//                country.setCode(CountryName.AUS.toCode());
+//                break;
+//            case "CHI":
+//                country.setCountryName(CountryName.CHI);
+//                country.setCode(CountryName.CHI.toCode());
+//                break;
+//            case "JPN":
+//                country.setCountryName(CountryName.JPN);
+//                country.setCode(CountryName.JPN.toCode());
+//                break;
+//            default :
+//                throw new Exception("Country not found");
+//        }
 
-//        country.setCountryName(CountryName.valueOf(countryName));
-//        country.setCode(CountryName.valueOf(countryName).toCode());
+//        country.setCountryName(CountryName.valueOf(s));
+//        country.setCode(CountryName.valueOf(s).toCode());
         country.setServiceProvider(serviceProvider);
 
         List<Country> countryList = serviceProvider.getCountryList();
